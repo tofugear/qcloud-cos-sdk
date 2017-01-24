0a6be060cba27e011631ea844b971fef82c36589
William Yeung <william@tofugear.com>
2017-01-24 16:23:43 +0800

    check if objectable has such assignment method first before trying to assign

--- lib/qcloud_cos/model/objectable.rb
+++ lib/qcloud_cos/model/objectable.rb
@@ -2,7 +2,7 @@ module QcloudCos
   module Objectable
     def initialize(hash)
       hash.each do |k, v|
-        send("#{k}=", v)
+        send("#{k}=", v) if respond_to?("#{k}=")
       end
     end
   end
